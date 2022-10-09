f51c08ab2791d118a8c12a44579cd98cabc1e45f
FdongFdong <abnormal5626@naver.com>
2022-10-10 00:15:10 +0900

    add

--- part2/date/src/App.js
+++ part2/date/src/App.js
@@ -1,9 +1,10 @@
 import MomentExample from './components/Moment/MomentExample';
 import DayjsExample from './components/Dayjs/DayjsExample';
-
+import DateFnsExample from './components/DateFns/DateFnsExample';
 function App() {
   return (
     <div className="App">
+      <DateFnsExample />
       <DayjsExample />
       <MomentExample />
     </div>
