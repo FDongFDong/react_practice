c7a3b00cc32700042f7b70e4c10c8c8c856e0899
FdongFdong <abnormal5626@naver.com>
2022-10-10 02:55:23 +0900

    fix

--- part2/styling/src/components/StyledComponentExample/StyledComponentExample.js
+++ part2/styling/src/components/StyledComponentExample/StyledComponentExample.js
@@ -1,39 +1,77 @@
 import React from 'react';
 
 import styled from 'styled-components';
+
+// 재사용성과 속도면에서 컴포넌트 바깥으로 빼는 것이 좋다.
+// -> 컴포넌트 안에 선언하면 매번 리렌더링되기 때문에 많이 느려짐
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Button = styled.button`
+  background: ${(props) => (props.primary ? 'palevioletred' : 'white')};
+  color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+const ReverseButton = (props) => (
+  <Button {...props} children={props.children.split('').reverse()} />
+);
+
+// &는 자기자신
+const Thing = styled.div.attrs(() => ({
+  tabIndex: 0,
+}))`
+  color: blue;
+
+  &:hover {
+    color: red;
+  }
+  & ~ & {
+    background: tomato;
+  }
+  & + & {
+    background: lime;
+  }
+  &.something {
+    background: orange;
+  }
+  .something-else & {
+    border: 1px solid;
+  }
+`;
+
 const StyledComponentExample = () => {
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-
-  const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-  `;
-
-  const Button = styled.button`
-    background: ${(props) => (props.primary ? 'palevioletred' : 'white')};
-    color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
-
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-  `;
-
-  const TomatoButton = styled(Button)`
-    color: tomato;
-    border-color: tomato;
-  `;
-
-  const ReverseButton = (props) => (
-    <Button {...props} children={props.children.split('').reverse()} />
-  );
   return (
     <>
+      <>
+        <Thing>Hello world!</Thing>
+        <Thing>How ya doing?</Thing>
+        <Thing className="something">The sun is shining...</Thing>
+        <div>Pretty nice day today.</div>
+        <Thing>Don't you think?</Thing>
+        <div className="something-else">
+          <Thing>Splendid.</Thing>
+        </div>
+      </>
+
       <Wrapper>
         <Title>Hello World!</Title>
       </Wrapper>
